12ab8bee16458453c90d8a41eb3c064cdefe6189
ksnaidu <ksnaidudba@gmail.com>
2026-07-17 12:48:13 +0530

    07-arrays.sh

--- shell-practice/07-arrays.sh
+++ shell-practice/07-arrays.sh
@@ -2,6 +2,10 @@
 
 MOVIES=("RRR" "salar" "peddi" "Animal")
 
-echo "FIRST movie: ${MOVIES[0]}"
+echo "FIRST movie: ${MOVIES[1]}"
 
 echo "LAST movie: ${MOVIES[3]}"
+
+echo "movies: ${MOVIES[4]}"
+
+echo "All movies: ${MOVIES[@]}"
