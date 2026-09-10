7acd4b077c208403db42f3bf63dd86334534433c
ksnaidu <ksnaidudba@gmail.com>
2026-09-10 13:05:03 +0530

    ec2

--- terraform/ec2/ec2.tf
+++ terraform/ec2/ec2.tf
@@ -1,34 +1,30 @@
 resource "aws_instance" "roboshop" {
-    ami = "ami-0220d79f3f480ecf5"
-    instance_type = "t3.micro"
+    ami = var.ami_id
+    instance_type = var.instance_type
     vpc_security_group_ids = [ aws_security_group.allow_all.id ]
 
-    tags = {
-        Name = "Helloworld"
-    }
+    tags = var.ec2_tags
   
 }
 
 resource "aws_security_group" "allow_all" {
-    name        = "allow_all"
-    description = "allow all traffic"
+    name        = var.sg_name
+    description = var.sg_description
 
     ingress {
-        from_port        = 0
-        to_port          = 0
+        from_port        = var.from_port
+        to_port          = var.to_port
         protocol         = "-1"
-        cidr_blocks      = ["0.0.0.0/0"]
+        cidr_blocks      = var.cidr_blocks
         ipv6_cidr_blocks = ["::/0"]
     }
     egress {
-        from_port        = 0
-        to_port          = 0
+        from_port        = var.from_port
+        to_port          = var.to_port
         protocol         = "-1"
-        cidr_blocks      = ["0.0.0.0/0"]
+        cidr_blocks      = var.cidr_blocks
         ipv6_cidr_blocks = ["::/0"]
     }
 
-    tags = {
-        Name = "allow-all"
-    }
+    tags = var.sg_tags
 }
