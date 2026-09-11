b8d05b4f63855b58c69c98131b50b4b53db70a28
ksnaidu <ksnaidudba@gmail.com>
2026-09-11 12:37:50 +0530

    loops

--- terraform/loops/ec2.tf
+++ terraform/loops/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "roboshop" {
-    count = 4
+    count=4
     ami = var.ami_id
     instance_type = var.environment == "dev" ? "t3.micro" : "t3.small"
     vpc_security_group_ids = [ aws_security_group.allow_all.id ]
