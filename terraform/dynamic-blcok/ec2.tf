33ee89f22ed418c23bdbbbfc332e98d89d6e7e29
ksnaidu <ksnaidudba@gmail.com>
2026-09-16 12:38:56 +0530

    dynamic-block

--- terraform/dynamic-blcok/ec2.tf
+++ terraform/dynamic-blcok/ec2.tf
@@ -1,21 +1,21 @@
+# Create an EC2 Instance
 resource "aws_instance" "roboshop" {
   #count=4
-  ami           = var.ami_id # Replace with a valid AMI ID for your region
-  for_each = var.instances
-  instance_type = each.value
-  vpc_security_group_ids = [ aws_security_group.allow_all.id ]
+  ami                    = var.ami_id # Replace with a valid AMI ID for your region
+  for_each               = var.instances
+  instance_type          = each.value
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
 
   tags = {
     Name = each.key
   }
 }
 
-
 resource "aws_security_group" "allow_all" {
-    name        = var.sg_name
-    description = var.sg_description
+  name        = var.sg_name
+  description = "allow all traffic"
 
-    dynamic "ingress" {
+  dynamic "ingress" {
     for_each = var.ingress_ports
     content {
       from_port        = ingress.value["from_port"]
@@ -25,17 +25,16 @@ resource "aws_security_group" "allow_all" {
       ipv6_cidr_blocks = ["::/0"]
     }
   }
-     
-       
-    }
-       
-    egress {
-        from_port        = var.from_port
-        to_port          = var.to_port
-        protocol         = "-1"
-        cidr_blocks      = var.cidr_blocks
-        ipv6_cidr_blocks = ["::/0"]
-    }
 
-    tags = var.sg_tags
+
+  egress {
+    from_port        = var.from_port
+    to_port          = var.to_port
+    protocol         = "-1"
+    cidr_blocks      = var.cidr_blocks
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = var.sg_tags
+
 }
--- terraform/dynamic-blcok/varaibles.tf
+++ terraform/dynamic-blcok/varaibles.tf
@@ -1,97 +1,89 @@
 variable "ami_id" {
-    type = string
-    default = "ami-0220d79f3f480ecf5"
-    description = "AMI id of RHEL9"
-  
+  type        = string
+  default     = "ami-0220d79f3f480ecf5"
+  description = "Ami id of RHEL9"
 }
-
 
 variable "instance_type" {
-    default = "t3.micro"
-  
-}
-
-variable "instances" {
-    default =  {
-        mongodb = "t3.micro"
-        redis = "t3.micro"
-        mysql = "t3.small"
-        rabbitmq = "t3.micro"
-    }
-  
+  default = "t3.micro"
 }
 
 variable "ec2_tags" {
   type = map(string)
   default = {
-    Name = "roboshop"
-    purpose = "varaibles-demo"
+    name    = "roboshop"
+    purpose = "variables-demo"
+
   }
 }
 
 variable "sg_name" {
-    default = "allow-all"
-  
-}
+  default = "allow-all-1"
 
-variable "sg_description" {
-    default = "allowing all ports from internt"
-  
 }
 
 variable "from_port" {
-   default = 0  
+  default = 0
 }
 
 variable "to_port" {
-    default = 0
-  
+  type    = number
+  default = 0
+
 }
 
 variable "cidr_blocks" {
-    type =  list(string)
-    default = ["0.0.0.0/0"]
-  
+  type    = list(string)
+  default = ["0.0.0.0/0"]
 }
 
 variable "sg_tags" {
-    default = {
-        Name = "allow-all"
-    }
-  
+  default = {
+    Name = "allow-all"
+  }
 }
 
 variable "environment" {
-    default = "dev"
-  
+  default = "dev"
+
 }
 
+variable "instances" {
+  #default = ["mongodb", "redis", "mysql", "rabbitmq"]
+  default = {
+    mongodb  = "t3.micro" # Each keyword is assgined for every iteration,you will get each.key and each.value.
+    redis    = "t3.micro"
+    mysql    = "t3.small"
+    rabbitmq = "t3.micro"
+  }
+
+}
 variable "zone_id" {
-    default = "Z08722562WXEO2CUTN2NL"
-  
+  default = "Z06008633JIHZ67B3RC4Q"
 }
 
 variable "domain-name" {
-    default = "kimidi.site"
-  
+  default = "kimidi.site"
+
 }
 
 variable "ingress_ports" {
-    default = [#list(map)
-    {
+    default = [ # list(map)
+        {
         from_port = 22
         to_port = 22
-    }
-    ,
+    },
+
     {
         from_port = 80
         to_port = 80
+  
+    },
 
-    }
-    ,
-    {
-        from_port = 8080
-        to_port = 8080
-    }
+   {
+    from_port = 8080
+    to_port = 8080
+
+   }
     ]
 }
