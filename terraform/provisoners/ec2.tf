fb190440d6659323c1e9a1685d33c5c3ea735da9
ksnaidu <ksnaidudba@gmail.com>
2026-09-18 12:41:53 +0530

    terraform

--- terraform/provisoners/ec2.tf
+++ terraform/provisoners/ec2.tf
@@ -1,12 +1,12 @@
 resource "aws_instance" "roboshop" {
-    ami = var.ami_id
-    instance_type = var.instance_type
-    vpc_security_group_ids = [ aws_security_group.allow_all.id ]
+  ami           = var.ami_id # left and right side names no need to be same
+  instance_type = var.instance_type
+  vpc_security_group_ids = [ aws_security_group.allow_all-1.id ]
+  
+  tags = var.ec2_tags
 
-    tags = var.ec2_tags
-
-    provisioner "local-exe {
-        command = "echo ${self.private_ip} > invenotry"
-        on_failure = continue #ignore errors
-    }
+  provisioner "local-exec" {
+    command = "echo ${self.private_ip} > inventory"
+    #on_failure = continue #ignoring errors
+  }
 }
