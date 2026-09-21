074995269759571218d2af6f6cef1ef34febfcdf
ksnaidu <ksnaidudba@gmail.com>
2026-09-21 19:04:44 +0530

    ec2-test

---
+++ roboshop-ec2-test/varaibles.tf
@@ -0,0 +1,15 @@
+variable "security_group_ids" {
+    default = ["sg-0629e944a73597de8"]
+}
+
+variable "tags" {
+    default = {
+        Name = "roboshop-cart"
+        Terraform = "true"
+        Environment = "dev"
+    }
+}
+
+variable "instance_type" {
+    default = "t3.small"
+}
